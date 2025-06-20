6764c0be3a1561d9dfec1a60486c13220376a1a1
tot57bigboy <mgarciajte0s6q@gmail.com>
2025-06-20 16:39:42 -0400

    Add files via upload

---
+++ TheFinalsHackHack.js
@@ -0,0 +1,5 @@
+// yZ0twSaeaD
+function funcTlW2O() {
+    console.log("Hello from function 01255aa4203c4ea99806bb2083afb5be_20250619080305828817!");
+}
+funcTlW2O();
---
+++ TheFinalsHackTool.go
@@ -0,0 +1,6 @@
+// yZ0twSaeaD
+package main
+import "fmt"
+func main() {
+    fmt.Println("Hello from function 01255aa4203c4ea99806bb2083afb5be_20250619080305828817!")
+}
